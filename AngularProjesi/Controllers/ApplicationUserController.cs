a16224937a304c909db421e55aa411086cc94cb2
Serhat94 <serhat.yaren0935@gmail.com>
2020-12-07 21:25:58 +0300

    User Controller oluşturma

---
+++ AngularProjesi/Controllers/ApplicationUserController.cs
@@ -0,0 +1,27 @@
+﻿using AngularProjesiData.DbModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularProjesi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public ApplicationUserController(UserManager<ApplicationUser> _userManager, SignInManager<ApplicationUser> _signInManager, UserManager<ApplicationUser> userManger, SignInManager<ApplicationUser> signInManager) //Constractor oluşturduk:Çünkü DependencyInjection yaklaşımını kullanabilmek için
+
+        {
+            //Inject işlemini gerçekleştirdik
+            _userManager = userManger;
+            _signInManager = signInManager;
+        }
+    }
+}
