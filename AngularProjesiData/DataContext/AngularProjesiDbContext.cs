b6edb97e53228a0ccd730cc4620357c0e1575d8b
Serhat94 <serhat.yaren0935@gmail.com>
2020-12-08 17:28:28 +0300

    Entity FrameWorklerimizi oluşturma veritabanın hazır hale gelmesi

--- AngularProjesiData/DataContext/AngularProjesiDbContext.cs
+++ AngularProjesiData/DataContext/AngularProjesiDbContext.cs
@@ -12,5 +12,9 @@ namespace AngularProjesiData.DataContext
 
         }
         public DbSet<ApplicationUser> ApplicationUsers{ get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
---
+++ AngularProjesiData/DbModels/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AngularProjesiData.DbModels
+{
+   public class Item
+    {
+        [Key]
+        public int ItemId { get; set; }
+        
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
