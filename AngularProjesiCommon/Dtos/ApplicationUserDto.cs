96c58229ed358af83a20a9d30d5fedfa03e91f36
Serhat94 <serhat.yaren0935@gmail.com>
2020-12-08 12:30:13 +0300

    ApplicationUserDto classı oluşturuldu

--- AngularProjesiCommon/Dtos/ApplicationUserDto.cs
+++ AngularProjesiCommon/Dtos/ApplicationUserDto.cs
@@ -9,7 +9,7 @@ namespace AngularProjesiCommon.Dtos
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string FullName { get; set; }
+        public string FullName { get; set; } //Veritabanı kısmına otomatik gönderme
 
     }
 }
